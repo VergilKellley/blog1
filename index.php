a678710298ba1f7271e7fd521f4201d43bc5833a
Vergil Kelley <verg_1@yahoo.com>
2018-07-13 14:35:24 -0500

    added include header.php

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie-edge">
-  <meta name="description" content="">
-  <meta name="keywords" content="">
-  <meta name="author" content="Vergil Kelley">
-
-    <link href="css/styles.css" type="text/css" rel="stylesheet"/>
-    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://unpkg.com/popper.js/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    
-  </head>
-  <body>
 <? php include "header.php"; 
 ?>
 
@@ -40,7 +21,7 @@
               <div>
                 <h2 id="paris-france">Paris France</h2>
                 <p id="city-of-lights">City of Lights</p>
-                <p id="paris-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                <p id="paris-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut.</p>
               </div>
